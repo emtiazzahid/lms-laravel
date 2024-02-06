632fa023891d81651ef94bdcaf34b34e0707615b
Emtiaz Zahid <emtiazzahid@gmail.com>
2024-02-06 09:49:23 +0600

    asset fix

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -6,7 +6,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+    
     <title>@yield('title')</title>
 
     @include('admin.layouts.css')
